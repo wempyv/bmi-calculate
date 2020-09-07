a8c3357869f3f039ca4ede6ca9fb62f27619930b
Wempy Virgana <43679344+wempyv@users.noreply.github.com>
2020-09-07 15:46:56 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -34,14 +34,14 @@ function calculate(results, yourName) {
         good luck and recalculate your body mass index 😉</p>`
     } else if (results >= 18.5 && results <= 22.9) {
         result.innerHTML = `<p class="content">Hi ${yourName}, your body mass index
-        score is <span>${results}</span> , Your weight is normal  🥳</p>`
+        score is <span>${results}</span> , Your weight is normal 🤗</p>`
 
         tips.innerHTML = `<p class="content">${yourName} , you must maintain a healthy weight, exercise regularly, maintain a healthy diet, and get adequate rest. stay happy ${yourName} and always lucky 😉</p>`
     } else if (results >= 23 && results <= 29.9) {
         result.innerHTML = `<p class="content">Hi ${yourName}, your body mass index
         score is <span>${results}</span> , it means you are overweight 😔</p>`
 
-        tips.innerHTML = `<p class="content">${yourName} , you have to get more exercise, reduce calories in the food you eat, reduce sweet foods. I hope your weight returns to normal, good luck ${yourName} and don't forget to count your body index again 🥰</p>`
+        tips.innerHTML = `<p class="content">${yourName} , you have to get more exercise, reduce calories in the food you eat, reduce sweet foods. I hope your weight returns to normal, good luck ${yourName} and don't forget to count your body index again 🤗</p>`
     } else {
         result.innerHTML = `<p class="content">Hi ${yourName}, your body mass index
         score is <span>${results}</span> , it means you are obese 😔</p>`
